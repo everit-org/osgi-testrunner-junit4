f7e4df4ca33a27e953fd4fcd578fa5dba7608837
Balazs Zsoldos <balazs.zsoldos@everit.biz>
2017-01-03 00:35:02 +0100

    Upgrading to testrunner 5.0.0 and-SNAPSHOT and new parent

--- src/main/java/org/everit/osgi/dev/testrunner/junit4/internal/DevelopmentModeFilter.java
+++ src/main/java/org/everit/osgi/dev/testrunner/junit4/internal/DevelopmentModeFilter.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright (C) 2011 Everit Kft. (http://www.everit.org)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.everit.osgi.dev.testrunner.junit4.internal;
 
 import org.everit.osgi.dev.testrunner.TestDuringDevelopment;
@@ -11,8 +26,8 @@ public class DevelopmentModeFilter extends Filter {
 
   @Override
   public String describe() {
-    return "In case the development mode flag is true, a method annotated with @Test will run only if the "
-        + "@TestMethodInDevelopmentMode is present or the class is annotated with @TestClassInDevelopmentMode";
+    return "In case the development mode flag is true, a method annotated with @Test will"
+        + " run only if the method or the class is annotated with @TestDuringDevelopment";
   }
 
   @Override
